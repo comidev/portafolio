29d3b3e622e211027c2297596a7d7a94d90efe78
Omar Miranda <comidev.contacto@gmail.com>
2022-06-11 22:45:25 -0500

    agregando conocimiento

--- js/data/conocimientos.js
+++ js/data/conocimientos.js
@@ -1,36 +1,3 @@
-const frontend = [
-    {
-        urlImagen: "./assets/images/conocimiento/html.png",
-        titulo: "HTML",
-        nivel: "Intermedio",
-    },
-    {
-        urlImagen: "./assets/images/conocimiento/css.png",
-        titulo: "CSS",
-        nivel: "Intermedio",
-    },
-    {
-        urlImagen: "./assets/images/conocimiento/javascript.png",
-        titulo: "JavaScript",
-        nivel: "Intermedio",
-    },
-    {
-        urlImagen: "./assets/images/conocimiento/react.png",
-        titulo: "React",
-        nivel: "Intermedio",
-    },
-    {
-        urlImagen: "./assets/images/conocimiento/styledComponent.png",
-        titulo: "Styled Component",
-        nivel: "Intermedio",
-    },
-    {
-        urlImagen: "./assets/images/conocimiento/redux.png",
-        titulo: "Redux",
-        nivel: "Básico",
-    },
-];
-
 const backend = [
     {
         urlImagen: "./assets/images/conocimiento/java.png",
@@ -65,6 +32,54 @@ const backend = [
     {
         urlImagen: "./assets/images/conocimiento/jwt.png",
         titulo: "JWT",
+        nivel: "Básico",
+    },
+    {
+        urlImagen: "./assets/images/conocimiento/mongodb.png",
+        titulo: "MongoDB",
+        nivel: "Básico",
+    },
+    {
+        urlImagen: "./assets/images/conocimiento/postgresql.png",
+        titulo: "Postgre SQL",
+        nivel: "Básico",
+    },
+    {
+        urlImagen: "./assets/images/conocimiento/springCloud.png",
+        titulo: "Spring Cloud",
+        nivel: "Básico",
+    },
+];
+
+const frontend = [
+    {
+        urlImagen: "./assets/images/conocimiento/html.png",
+        titulo: "HTML",
+        nivel: "Intermedio",
+    },
+    {
+        urlImagen: "./assets/images/conocimiento/css.png",
+        titulo: "CSS",
+        nivel: "Intermedio",
+    },
+    {
+        urlImagen: "./assets/images/conocimiento/javascript.png",
+        titulo: "JavaScript",
+        nivel: "Intermedio",
+    },
+    {
+        urlImagen: "./assets/images/conocimiento/react.png",
+        titulo: "React",
+        nivel: "Intermedio",
+    },
+    {
+        urlImagen: "./assets/images/conocimiento/styledComponent.png",
+        titulo: "Styled Component",
+        nivel: "Intermedio",
+    },
+    {
+        urlImagen: "./assets/images/conocimiento/redux.png",
+        titulo: "Redux",
         nivel: "Básico",
     },
 ];
